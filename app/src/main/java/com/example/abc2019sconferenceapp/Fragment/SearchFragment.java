a7488cadcfdf55d4d015b16d9b41cdd7cd9913e7
Yuki014 <b.d.cloud2385@gmail.com>
2019-03-21 13:09:25 +0900

    検索するボタンをタップしたときに画面遷移するように変更

--- app/src/main/java/com/example/abc2019sconferenceapp/Fragment/SearchFragment.java
+++ app/src/main/java/com/example/abc2019sconferenceapp/Fragment/SearchFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.abc2019sconferenceapp.R;
 
@@ -20,6 +22,41 @@ public class SearchFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         // 先ほどのレイアウトをここでViewとして作成します
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        Button searchButton = getActivity().findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new SearchResultFragment()).commit();
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
 }
