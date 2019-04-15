2902488ac13624ade16ab41a566a793db6d86352
Yuki014 <b.d.cloud2385@gmail.com>
2019-04-15 17:59:20 +0900

    その他ページの項目をタップしたときの処理を追加

--- app/src/main/java/com/example/abc2019sconferenceapp/Fragment/OtherFragment.java
+++ app/src/main/java/com/example/abc2019sconferenceapp/Fragment/OtherFragment.java
@@ -1,20 +1,75 @@
 package com.example.abc2019sconferenceapp.Fragment;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.abc2019sconferenceapp.R;
 
-public class OtherFragment extends Fragment {
+public class OtherFragment extends Fragment{
+    TextView aboutABC;
+    TextView notification;
+    TextView map;
+    TextView survey;
+    TextView inquiry;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        // 先ほどのレイアウトをここでViewとして作成します
-        return inflater.inflate(R.layout.fragment_other, container, false);
+        View v = inflater.inflate(R.layout.fragment_other, container, false);
+        aboutABC = v.findViewById(R.id.about_abc);
+        aboutABC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFragment("https://abc.android-group.jp/2018s/about/");
+            }
+        });
+        notification = v.findViewById(R.id.notification);
+        notification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        map = v.findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        survey = v.findViewById(R.id.survey);
+        survey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        inquiry = v.findViewById(R.id.Inquiry);
+        inquiry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFragment("https://abc.android-group.jp/2018s/contact/");
+            }
+        });
+        return v;
+    }
+
+    private void sendFragment(String url) {
+        FragmentManager fragmentManager = getFragmentManager();
+        Bundle bundle = new Bundle();
+        bundle.putString("url", url);
+        WebViewFragment webViewFragment = new WebViewFragment();
+        webViewFragment.setArguments(bundle);
+        if (fragmentManager != null) {
+            fragmentManager.beginTransaction().add(R.id.setFragmentLayout, webViewFragment).addToBackStack(null).commit();
+        }
+
     }
 
 }
---
+++ app/src/main/java/com/example/abc2019sconferenceapp/Fragment/WebViewFragment.java
@@ -0,0 +1,65 @@
+package com.example.abc2019sconferenceapp.Fragment;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.example.abc2019sconferenceapp.R;
+
+public class WebViewFragment extends Fragment {
+
+    String url = "";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View v = inflater.inflate(R.layout.webview_item, container, false);
+
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            url = bundle.getString("url");
+        }
+
+        final WebView webView = v.findViewById(R.id.webView);
+
+        webView.loadUrl(url);
+        webView.setWebViewClient(new WebViewClient()
+        {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url)
+            {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon)
+            {
+                /*ページの読み込みが始まった時の処理*/
+                Log.i("MainActivity : ", "読み込み開始");
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url)
+            {
+                /*ページの読み込みが終わった時の処理*/
+                Log.i("MainActivity : ", "読み込み終了");
+            }
+
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description,
+                                        String failingUrl)
+            {
+                /*読み込めなかったときの処理*/
+                Log.i("MainActivity : ", "読み込み失敗");
+            }
+        });
+
+        return v;
+    }
+}
