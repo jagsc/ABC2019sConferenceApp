41a80e2980c6540fa05ee2f400f6cadc001238c5
Fumiya Kume <fumiya.kume@hotmail.com>
2019-05-04 20:13:48 +0900

    Firebase-Crashes のところで落ちる不具合を修正

--- app/src/main/java/com/example/abc2019sconferenceapp/MainActivity.java
+++ app/src/main/java/com/example/abc2019sconferenceapp/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.abc2019sconferenceapp;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.abc2019sconferenceapp.Fragment.FavoriteFragment;
@@ -16,42 +14,45 @@ import com.example.abc2019sconferenceapp.Fragment.TimelineFragment;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-    private FirebaseAnalytics mFirebaseAnalytics;
+  private FirebaseAnalytics mFirebaseAnalytics;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        Bundle bundle = new Bundle();
-        bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id");//TODO アプリのIDに変える
-        bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name");//TODD アプリのnameに変える
-        bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
-        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
-
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                switch (menuItem.getItemId()) {
-                    case R.id.timeline:
-                        fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new TimelineFragment()).commit();
-                        return true;
-                    case R.id.search:
-                        fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new SearchFragment()).commit();
-                        return true;
-                    case R.id.favorite:
-                        fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new FavoriteFragment()).commit();
-                        return true;
-                    case R.id.other:
-                        fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new OtherFragment()).commit();
-                        return true;
-                }
-                return false;
-            }
-        });
-
+    Bundle bundle = new Bundle();
+    bundle.putString(FirebaseAnalytics.Param.ITEM_ID, "id");//TODO アプリのIDに変える
+    bundle.putString(FirebaseAnalytics.Param.ITEM_NAME, "name");//TODD アプリのnameに変える
+    bundle.putString(FirebaseAnalytics.Param.CONTENT_TYPE, "image");
+    if (mFirebaseAnalytics != null) {
+      mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.SELECT_CONTENT, bundle);
     }
+
+
+    BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+    bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+      @Override
+      public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        switch (menuItem.getItemId()) {
+          case R.id.timeline:
+            fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new TimelineFragment()).commit();
+            return true;
+          case R.id.search:
+            fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new SearchFragment()).commit();
+            return true;
+          case R.id.favorite:
+            fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new FavoriteFragment()).commit();
+            return true;
+          case R.id.other:
+            fragmentManager.beginTransaction().replace(R.id.setFragmentLayout, new OtherFragment()).commit();
+            return true;
+        }
+        return false;
+      }
+    });
+
+  }
 }
 
