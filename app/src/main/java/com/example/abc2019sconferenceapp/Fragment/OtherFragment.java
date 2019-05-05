123378af5710222cc980fbe10db4cfb6543b435c
Yuki014 <b.d.cloud2385@gmail.com>
2019-05-05 14:40:09 +0900

    その他ページのURLを今年のページに変更

--- app/src/main/java/com/example/abc2019sconferenceapp/Fragment/OtherFragment.java
+++ app/src/main/java/com/example/abc2019sconferenceapp/Fragment/OtherFragment.java
@@ -14,9 +14,10 @@ import com.example.abc2019sconferenceapp.R;
 
 public class OtherFragment extends Fragment{
     TextView aboutABC;
-    TextView notification;
+    TextView blog;
     TextView map;
-    TextView survey;
+    TextView socialGathering;
+    TextView registration;
     TextView inquiry;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,35 +27,42 @@ public class OtherFragment extends Fragment{
         aboutABC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendFragment("https://abc.android-group.jp/2018s/about/");
+                sendFragment("https://abc.android-group.jp/2019s/about");
             }
         });
-        notification = v.findViewById(R.id.notification);
-        notification.setOnClickListener(new View.OnClickListener() {
+        blog = v.findViewById(R.id.blog);
+        blog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sendFragment("https://abc.android-group.jp/2019s/blog/");
             }
         });
         map = v.findViewById(R.id.map);
         map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sendFragment("https://www.u-tokai.ac.jp/info/traffic_map/shared/pdf/takanawa_campus.pdf");
             }
         });
-        survey = v.findViewById(R.id.survey);
-        survey.setOnClickListener(new View.OnClickListener() {
+        socialGathering = v.findViewById(R.id.socialGathering);
+        socialGathering.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                sendFragment("https://abc2019s-party.peatix.com/");
+            }
+        });
+        registration = v.findViewById(R.id.registration);
+        registration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendFragment("https://japan-android-group.connpass.com/event/125928/");
             }
         });
         inquiry = v.findViewById(R.id.Inquiry);
         inquiry.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendFragment("https://abc.android-group.jp/2018s/contact/");
+                sendFragment("https://abc.android-group.jp/2019s/contact/");
             }
         });
         return v;
